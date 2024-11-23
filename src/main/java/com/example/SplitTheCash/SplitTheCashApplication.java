cbe5599e543321063f379eaf6b259d9090bd6210
Sagar Chowdhury <sagarchowdhury4693@gmail.com>
2024-11-23 15:05:13 +0530

    Initial Commit

---
+++ src/main/java/com/example/SplitTheCash/SplitTheCashApplication.java
@@ -0,0 +1,13 @@
+package com.example.SplitTheCash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitTheCashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitTheCashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SplitTheCash/SplitTheCashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SplitTheCash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitTheCashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
